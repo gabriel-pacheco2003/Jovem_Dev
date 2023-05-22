0c5966f7eb9cc35ca01529d55c2c06ab7ae32248
Gabriel Pacheco <lupusgabriel12@gmail.com>
2023-05-22 15:51:11 -0300

    segundo commit

--- src/main/java/br/com/trier/aula_01/HelloWorld.java
+++ src/main/java/br/com/trier/aula_01/HelloWorld.java
@@ -3,7 +3,8 @@ package br.com.trier.aula_01;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
+		System.out.println("Olá Mund!");
+		System.out.println("**********************");
 	}
 
 }
