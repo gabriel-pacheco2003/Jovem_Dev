708df0b328abd7111e9d1992291acc42fb5fd392
Gabriel Pacheco <lupusgabriel12@gmail.com>
2023-06-06 17:30:19 -0300

    Continuação dos scripts

--- script/script_candidato.sql
+++ script/script_candidato.sql
@@ -39,16 +39,78 @@ inner join candidato c on c.cidade = cid.id and cid.nome = 'TUBARÃO'
 inner join cargo on cargo.id = c.cargo and cargo.nome = 'Prefeito' 
 order by c.nome; 
 
-select c.nome, cid.qt-eleitores
-from cidade cid
-inner join candidato c on cid.nome =(select max(qt_eleitores) from cidade)
+select c.nome
+from candidato c
 inner join cargo on cargo.id = c.cargo and cargo.nome = 'Prefeito'
+inner join cidade cid on cid.id = c.cidade and cid.qt_eleitores = (select max(qt_eleitores) from cidade)
 order by c.nome;
 
-select c.cidade , cidade.nome 
+select count(*)
 from candidato c
-inner join cidade.nome on c.cidade = cidade.id and cidade.nome = 'TUBARÃO'
 inner join cargo on cargo.id = c.cargo and cargo.nome = 'Vereador'
-inner join 
+inner join cidade on c.cidade = cidade.id and cidade.nome = 'TUBARÃO'; 
 
-select count(*) from candidato c where 
+select cidade.nome, count(*)
+from candidato c 
+inner join cargo on cargo.id = c.cargo and cargo.nome = 'Vereador'
+inner join cidade on c.cidade = cidade.id 
+group by cidade.nome 
+order by cidade.nome;
+
+select cargo.nome, vi.nulos, vi.brancos
+from cargo 
+inner join voto_invalido vi on vi.cargo = cargo.id 
+inner join cidade on cidade.id = vi.cidade and cidade.nome = 'TUBARÃO'; 
+
+select vi.nulos + vi.brancos as total
+from cargo 
+inner join voto_invalido vi on vi.cargo = cargo.id and cargo.nome = 'Prefeito' 
+inner join cidade on cidade.id = vi.cidade and cidade.nome = 'TUBARÃO';
+
+select candidato.nome, voto.voto
+from candidato 
+inner join voto on voto.candidato = candidato.id 
+inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Prefeito'
+inner join cidade on cidade.id = candidato.cidade and cidade.nome = 'TUBARÃO'
+order by voto desc;
+
+select candidato.nome, voto.voto as votos
+from candidato 
+inner join voto on voto.candidato = candidato.id 
+inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Vereador'
+inner join cidade on cidade.id = candidato.cidade and cidade.nome = 'TUBARÃO'
+order by votos desc;
+
+select cidade.nome, max(voto.voto)
+from cidade
+inner join candidato on candidato.cidade = cidade.id  
+inner join voto on voto.candidato = candidato.id
+inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Prefeito'
+group by cidade.nome;
+
+select partido.sigla, sum(voto.voto) as votos
+from partido 
+inner join candidato on candidato.partido = partido.id 
+inner join cidade on cidade.id = candidato.cidade and cidade.nome = 'TUBARÃO'
+inner join voto on voto.candidato = candidato.id
+group by partido.sigla 
+order by votos desc;
+
+select count(*)
+from 
+
+select count(*)
+from cidade c
+inner join voto on c.qt_eleitores - voto.voto and c.nome = 'TUBARÃO'
+inner join voto_invalido vi on c.qt_eleitores - (vi.brancos + vi.nulos);
+
+select count(*)
+
+//24
+
+select c.nome prefeito, cidade.nome cidade
+from candidato c 
+inner join cargo on cargo.id = c.cargo and cargo.nome = 'Prefeito'
+inner join cidade on cidade.id = c.cidade
+inner join voto on voto.candidato = c.id and voto.voto = (select max(voto) from voto)
+order by cidade.nome;
